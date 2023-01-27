bfc3da8b70a22c4c2a27ece87914cbbc0edcd6a5
nasimqureshi <qureshi.nasim@yahoo.com>
2023-01-27 12:08:36 -0600

    commit

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 
 
 // if else if statements
-$age = 15;
+// $age = 15;
 
 // if($age >= 18)
 // if($age >= 19){
@@ -31,19 +31,36 @@ $age = 15;
 // else{
 //     echo "Yes you can vote";
 
-if($age >= 18){
-    echo "you can vote";
-}
-elseif($age == 17){
-    echo "you can not vote";
-}
-elseif($age == 16){
-    echo "you can not vote";
-}
-else
-{
-    echo "Your are under age";
-}
+// if($age >= 18){
+//     echo "you can vote";
+// }
+// elseif($age == 17){
+//     echo "you can not vote";
+// }
+// elseif($age == 16){
+//     echo "you can not vote";
+// }
+// else
+// {
+//     echo "Your are under age";
+// }
+// if($age >= 18){
+//     if($age < 66)
+//     echo "You can vote";
+// }
+// else {
+//     echo "you can not vote anymore";
+// }
+
+
+// Array
+//index array
+$arr1 = array("car1", "car2", "car3");
+// echo "<pre>";
+// print_r($arr1);
+// echo "</pre>"
+echo $arr1[2];
+
 
 
 

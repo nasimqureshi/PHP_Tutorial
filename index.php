1aa02d0eec79a73b019f582884d2e86ddfd16b14
nasimqureshi <qureshi.nasim@yahoo.com>
2023-01-27 11:24:19 -0600

    commit

--- index.php
+++ index.php
@@ -1,7 +1,26 @@
 <?php
 
-$firstName = "Nasim";
-$lastName = " Qureshi";
+// $firstName = "Nasim";
+// $lastName = " Qureshi";
 
-echo $firstName . $lastName ." how are you?"
+// echo $firstName . $lastName ." how are you?"
+
+
+// Addition:
+// $a = 15;
+// $b = 35;
+// $c = $a+$b;
+// echo "add: ".$c;
+// $c = $a-$b;
+// echo "\n subt: \t".$c;
+
+// Coparison Operators
+$x = 5;
+$y = 5;
+var_dump($x == $y);
+
+
+
+
+
 ?>
